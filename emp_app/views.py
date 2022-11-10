95dbc387bed83496c0260ae9a7612efff1811af5
SYN <SYN@mail.com>
2022-11-10 13:08:41 +0530

    Addded logic for saving form details in views.py

--- emp_app/views.py
+++ emp_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from .models import Employee, Role, Department
+from datetime import datetime
 
 def index(request):
     return render(request, 'index.html')
@@ -12,8 +13,33 @@ def view(request):
     }
     return render(request, 'view.html', context)
 
+
+
 def add(request):
-    return render(request, 'add.html')
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        dept = request.POST['dept']
+        salary = int(request.POST['salary'])
+        bonus = int(request.POST['bonus'])
+        role = request.POST['role']
+        phone = int(request.POST['phone'])
+        
+        hire_date = 0
+
+        if hire_date == None:
+            hire_date = datetime.now()
+        else:
+            hire_date = int(request.POST['hire_date'])
+
+        new_emp = Employee(first_name = first_name, last_name = last_name, dept_id = dept, role_id = role, salary = salary, bonus = bonus, phone = phone, hire_date = hire_date)
+
+        new_emp.save()
+
+        return HttpResponse("user created")
+    else:
+        return render(request, 'add.html')
+
 
 def filter(request):
     return render(request, 'filter.html')
