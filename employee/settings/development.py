df7c791e74114dc4d7c92b13296dec462ed01976
SYN <postbox.501@yandex.com>
2025-06-19 20:50:30 +0530

    added docker support and minor changes

--- employee/settings/development.py
+++ employee/settings/development.py
@@ -4,7 +4,7 @@ import os
 SECRET_KEY = "django-insecure-^&!qub_*@8)5v0o4=h_$$r(37k0-w^#+4^6fjl@108k_lu5_s+'"
 
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
